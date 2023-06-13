d050ff35555d555fc4c7e44e2de0e72d00df7881
Adam Getchell <acgetchell@ucdavis.edu>
2023-06-13 00:27:20 -0500

    Add tests, update blas_src

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate blas;
-extern crate openblas_src;
+extern crate blas_src;
 
 use blas::*;
 
@@ -16,4 +16,25 @@ fn main() {
     }
 
     assert!(c == vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0,]);
+}
+
+#[cfg(test)]
+mod test {
+    use blas::dgemm;
+
+    #[test]
+    fn it_works() {
+        let (m, n, k) = (2, 4, 3);
+        let a = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
+        let b = vec![
+            1.0, 5.0, 9.0, 2.0, 6.0, 10.0, 3.0, 7.0, 11.0, 4.0, 8.0, 12.0,
+        ];
+        let mut c = vec![2.0, 7.0, 6.0, 2.0, 0.0, 7.0, 4.0, 2.0];
+
+        unsafe {
+            dgemm(b'N', b'N', m, n, k, 1.0, &a, m, &b, k, 1.0, &mut c, m);
+        }
+
+        assert!(c == vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0,]);
+    }
 }
